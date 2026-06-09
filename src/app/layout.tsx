c7a9aa28adebefcb151e053948f66dea8e17c859
TioElvis <elvis.veramijares@gmail.com>
2026-06-09 21:32:51 +0200

    ----PORTFOLIO-WEB----

---
+++ src/app/layout.tsx
@@ -0,0 +1,49 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Cormorant_Garamond, JetBrains_Mono } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const cormorantGaramond = Cormorant_Garamond({
+  subsets: ["latin"],
+  variable: "--font-heading",
+});
+
+const jetBrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-geist-mono",
+});
+
+export const metadata: Metadata = {
+  creator: "Elvis Vera",
+  title: {
+    default: "TioElvis | Elvis Vera - Personal Portfolio",
+    template: "%s | TioElvis",
+  },
+  description:
+    "Personal portfolio created by Elvis Vera (TioElvis) where I document my projects, code, and continuous learning.",
+  keywords: [
+    "Elvis Vera",
+    "TioElvis",
+    "Software Development",
+    "Personal blog",
+    "Portfolio",
+    "Programming",
+    "Cybersecurity",
+    "Backend",
+  ],
+};
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: Props) {
+  return (
+    <html
+      lang="en"
+      className={cn(cormorantGaramond.variable, jetBrainsMono.variable)}>
+      <body>{children}</body>
+    </html>
+  );
+}
