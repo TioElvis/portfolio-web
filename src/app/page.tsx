859b3971f3c447af1b9a3c0e5ef9c5fdaa614eba
TioElvis <elvis.veramijares@gmail.com>
2026-06-11 09:46:45 +0200

    Create landing structure page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
 import { Fragment } from "react/jsx-runtime";
 
+import { Hero } from "@/components/landing/hero";
+import { About } from "@/components/landing/about";
+import { Header } from "@/components/landing/header";
+import { Contact } from "@/components/landing/contact";
+import { Projects } from "@/components/landing/projects";
+
 export default function Page() {
-  return <Fragment>Hello World</Fragment>;
+  return (
+    <Fragment>
+      <Header />
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+    </Fragment>
+  );
 }
