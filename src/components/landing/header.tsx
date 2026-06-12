57bccd9f3cdd146d22559970ddba16ceb9ecadd0
TioElvis <elvis.veramijares@gmail.com>
2026-06-12 10:03:04 +0200

    Change style on header

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -4,7 +4,7 @@ import { MaxWidthWrapper } from "@/components/max-width-wrapper";
 
 export function Header() {
   return (
-    <header className="w-full h-14 fixed border-b border-border bg-background/80 backdrop-blur-xl">
+    <header className="w-full h-14 fixed border-b border-border bg-background/60 backdrop-blur-xl z-50">
       <MaxWidthWrapper className="flex items-center justify-between">
         <TioElvisASCII />
         <Navbar />
