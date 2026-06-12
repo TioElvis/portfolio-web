bd8ab4785ef901971185eeaef224a47b6214df6c
TioElvis <elvis.veramijares@gmail.com>
2026-06-12 20:15:56 +0200

    Set more padding y in hero when is mobile

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -5,7 +5,7 @@ import { InteractiveShell } from "@/components/interactive-shell";
 
 export function Hero() {
   return (
-    <section id="hero" className="w-full h-screen py-20">
+    <section id="hero" className="w-full h-screen py-32 md:py-20">
       <MaxWidthWrapper className="flex flex-col items-center justify-center gap-4">
         <HeroText />
         <InteractiveShell />
