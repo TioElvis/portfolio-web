7da6b72c2ef157605b20c9b3db67191e69fecc9f
TioElvis <elvis.veramijares@gmail.com>
2026-06-13 13:21:37 +0200

    Change height style in hero section

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -5,7 +5,9 @@ import { InteractiveShell } from "@/components/interactive-shell";
 
 export function Hero() {
   return (
-    <section id="hero" className="w-full h-screen py-32 md:py-20">
+    <section
+      id="hero"
+      className="w-full h-[70vh] sm:h-[80vh] md:h-[90vh] lg:h-screen py-20">
       <MaxWidthWrapper className="flex flex-col items-center justify-center gap-4">
         <HeroText />
         <InteractiveShell />
