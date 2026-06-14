3234d5d75455b040bab14042f145a8a950ca8e41
TioElvis <elvis.veramijares@gmail.com>
2026-06-14 15:24:40 +0200

    Implement about section

--- src/components/landing/about.tsx
+++ src/components/landing/about.tsx
@@ -1,15 +1,88 @@
+"use client";
+import Image from "next/image";
+import { IconCode, IconLock, IconServer } from "@tabler/icons-react";
+
 import { ASCII_ABOUT, ASCII_ME } from "@/lib/ascii";
+
+import { useIsMobile } from "@/hooks/use-mobile";
 
 import { MaxWidthWrapper } from "@/components/max-width-wrapper";
 import { RenderTextASCII } from "@/components/render-text-ascii";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+
+const ABILITIES = [
+  {
+    name: "Frontend",
+    description: "React, Nextjs, TypeScript, Tailwind CSS, GSAP.",
+    Icon: IconCode,
+  },
+  {
+    name: "Backend",
+    description: "Nodejs, Nestjs, PostgreSQL, MongoDB.",
+    Icon: IconServer,
+  },
+  {
+    name: "Cybersecurity",
+    description: "Pentesting, System Defense, KaliLinux.",
+    Icon: IconLock,
+  },
+];
 
 export function About() {
+  const isMobile = useIsMobile();
+
   return (
-    <section id="about" className="w-full min-h-screen py-20">
-      <MaxWidthWrapper className="flex flex-col gap-4">
+    <section
+      id="about"
+      className="w-full min-h-screen scroll-mt-20 md:scroll-mt-0 md:flex md:justify-center md:items-center">
+      <MaxWidthWrapper className="flex flex-col items-center justify-center gap-4">
         <div className="flex items-center justify-center gap-4 text-[6px] sm:text-[7px] md:text-[8px] lg:text-[9px]">
           <RenderTextASCII ASCII_TEXT={ASCII_ABOUT} />
           <RenderTextASCII ASCII_TEXT={ASCII_ME} />
+        </div>
+        <div className="flex flex-col md:flex-row gap-4">
+          <div className="flex flex-1 flex-col gap-4">
+            <div className="flex flex-col gap-4">
+              <p>
+                I am a Venezuelan guy, born in Caracas that growing up
+                discovered his passion for computer science and application
+                development. In the future, I want to keep learning new things
+                in order to continuously sharpen my skills and knowledge.
+              </p>
+              <p>
+                So I use this portfolio/blog to documentate my projects and
+                share my knowledge.
+              </p>
+            </div>
+            <section className="flex flex-col gap-4">
+              {ABILITIES.map(({ name, description, Icon }) => {
+                return (
+                  <Card key={name} className="hover:ring-primary/50">
+                    <CardHeader>
+                      <Icon className="size-5 text-primary" />
+                    </CardHeader>
+                    <CardContent>
+                      <h3 className="text-sm font-bold">{name}</h3>
+                      <p className="text-xs">{description}</p>
+                    </CardContent>
+                  </Card>
+                );
+              })}
+            </section>
+          </div>
+          {!isMobile && (
+            <div className="flex h-auto items-center justify-center flex-1 relative">
+              <article className="relative w-full h-full">
+                <Image
+                  src="/evera.png"
+                  alt="evera"
+                  fill
+                  className="object-cover"
+                  priority
+                />
+              </article>
+            </div>
+          )}
         </div>
       </MaxWidthWrapper>
     </section>
