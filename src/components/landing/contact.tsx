757b0838955b7a078d7414107a3fba9d0c1b48e4
TioElvis <elvis.veramijares@gmail.com>
2026-06-14 20:41:49 +0200

    FInish contact section and landing page

--- src/components/landing/contact.tsx
+++ src/components/landing/contact.tsx
@@ -1,18 +1,74 @@
-import { ASCII_CONTACT } from "@/lib/ascii";
+import Link from "next/link";
+import {
+  IconBrandGithub,
+  IconBrandLinkedin,
+  IconBrandYoutube,
+} from "@tabler/icons-react";
 
+import { ASCII_CONTACT, ASCII_ME, ASCII_PENGUIN } from "@/lib/ascii";
+
+import { Button } from "@/components/ui/button";
 import { MaxWidthWrapper } from "@/components/max-width-wrapper";
 import { RenderTextASCII } from "@/components/render-text-ascii";
 
+const CONTACTS = [
+  {
+    name: "Github",
+    icon: IconBrandGithub,
+    to: "https://github.com/TioElvis",
+  },
+  {
+    name: "Linkedin",
+    icon: IconBrandLinkedin,
+    to: "https://www.linkedin.com/in/elvis-vera-3657ba365/",
+  },
+  {
+    name: "Youtube",
+    icon: IconBrandYoutube,
+    to: "https://www.youtube.com/@tioelvis4615",
+  },
+];
+
 export function Contact() {
   return (
-    <footer id="contact" className="w-full min-h-screen scroll-mt-20">
-      <MaxWidthWrapper>
+    <footer id="contact" className="w-full scroll-mt-20 md:scroll-mt-0 py-12">
+      <MaxWidthWrapper className="flex flex-col items-center gap-4">
         <div className="flex items-center justify-center gap-4">
           <RenderTextASCII
             ASCII_TEXT={ASCII_CONTACT}
             className="text-[4px] sm:text-[6px] md:text-[7px] lg:text-[8px]"
           />
+          <RenderTextASCII
+            ASCII_TEXT={ASCII_ME}
+            className="text-[4px] sm:text-[6px] md:text-[7px] lg:text-[8px]"
+          />
         </div>
+        <div className="flex flex-col items-center gap-4 w-full md:w-96">
+          <div className="w-full flex flex-col gap-4">
+            {CONTACTS.map((e) => {
+              return (
+                <Button
+                  key={e.name}
+                  nativeButton={false}
+                  render={
+                    <Link href={e.to} target="_blank">
+                      {e.name}
+                    </Link>
+                  }
+                />
+              );
+            })}
+          </div>
+          <div className="flex-1 p-4 rounded-md">
+            <RenderTextASCII ASCII_TEXT={ASCII_PENGUIN} />
+          </div>
+        </div>
+        <section className="text-center">
+          <p>
+            &copy; {new Date().getFullYear()} TioElvis. All rights reserved.
+          </p>
+          <p>Built with ❤️ and lots of ☕ by TioElvis</p>
+        </section>
       </MaxWidthWrapper>
     </footer>
   );
